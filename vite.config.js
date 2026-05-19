2f9f68bf75d1896bc3e8e4c999552e727d85a8cb
畑 大二郎 (FD推進部) <dhata@bbs.co.jp>
2026-05-19 12:24:08 +0900

    fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dx-portal-project/",
+  base: "/dx-portal-project_/",
   plugins: [react()],
   server: {
     port: 5173,
